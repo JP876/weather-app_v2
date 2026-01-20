246a84d37128ff9f832ca15c2163760ffec070da
JosipP <popovicjosip867@gmail.com>
2026-01-20 16:24:26 +0100

    Added fuzzy city search

--- apps/client/src/WeatherForecast/AddCity/CitySearch.tsx
+++ apps/client/src/WeatherForecast/AddCity/CitySearch.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useMemo, useRef, useState } from "react";
 import { TextField } from "@mui/material";
+import Fuse from "fuse.js";
+import { useAtom } from "jotai";
+
+import { citiesAtom } from "../../atoms";
+import type { CityType } from "../../types";
 
 const CitySearch = () => {
-    return <TextField size="small" variant="outlined" placeholder="Search city..." fullWidth />;
+    const [value, setValue] = useState("");
+    const initialCities = useRef<CityType[] | null>(null);
+
+    const fuse = useMemo(() => {
+        return new Fuse<CityType>([], {
+            includeScore: true,
+            includeMatches: true,
+            threshold: 0.4,
+            keys: [
+                { name: "city", weight: 1 },
+                { name: "country", weight: 0.8 },
+            ],
+        });
+    }, []);
+
+    const [cities, setCities] = useAtom(citiesAtom);
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setValue(value);
+
+        if (!value) {
+            setCities(initialCities.current);
+        } else {
+            const results = fuse.search(value);
+            console.log(results);
+            setCities(results.map((el) => ({ ...el.item })));
+        }
+    };
+
+    useEffect(() => {
+        if (initialCities.current === null && Array.isArray(cities)) {
+            initialCities.current = cities;
+            fuse.setCollection(cities);
+        }
+    }, [cities, fuse]);
+
+    return (
+        <TextField
+            size="small"
+            variant="outlined"
+            placeholder="Search city..."
+            fullWidth
+            value={value}
+            onChange={handleOnChange}
+        />
+    );
 };
 
 export default CitySearch;
